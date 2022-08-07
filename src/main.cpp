eb2d74316182daec89dbaeac7f9cc2a0cb49306e
Farhana-Jamal <farhanajamaludeen@gmail.com>
2022-08-07 17:31:15 +0530

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,56 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-int LED = 2;
-void setup() {
-  pinMode(LED,OUTPUT);
-  Serial.begin(115200);
- 
-  // put your setup code here, to run once:
+const char* ssid = "Farhana";
+const char* password = "1234567890";
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid , password);
+  Serial.println("Connecting to wifi network");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  digitalWrite(LED,HIGH);
+void setup() 
+{
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RSSI : ");
+  Serial.println(WiFi.RSSI());
   delay(1000);
-  digitalWrite(LED,LOW);
-  delay(1000);
-   Serial.println("hello world");
-   Serial.print("gjdftfygj");
-  // put your main code here, to run repeatedly:
+ 
+}
+
+void loop() 
+{
+    Serial.println("scan start");
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n==0)
+  {
+    Serial.println("no networks found");
+  }
+  else
+  {
+    Serial.println("networks found:");
+    Serial.println(n);
+    for (int i = 0; i < n; i++)
+    {
+      Serial.print(i + 1);
+      Serial.print(":");
+      Serial.print(WiFi.SSID(i));
+      Serial.print("(");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)? "" : "*");
+      delay(100);
+    }
+  }
+   Serial.println("*");
+   delay(100);
 }
